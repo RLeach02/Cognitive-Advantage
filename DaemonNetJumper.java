e38a7fb18048f3826fe0b9593145acfc355304bc
Chitipat Marsri <116041516+Pat-chiti@users.noreply.github.com>
2023-05-25 09:04:58 +1000

    Update DaemonNetJumper.java

--- DaemonNetJumper.java
+++ DaemonNetJumper.java
@@ -1,5 +1,7 @@
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 /**
  * Class: DaemonNetJumper.java
  * Description: A class that provide a daemon for NetJumper
@@ -9,20 +11,35 @@ import java.util.Scanner;
  * @LastUpdate: 24 May 2023
  */
 public class DaemonNetJumper {
+    /** 
+    * Method: main 
+    * Description: Runs NetJumper
+    * @param args
+    */ 
     public static void main(String args[]) {
-        String ipAddr = ;
-        String user = ;
-        String password = ;
+        //read file
+        Properties properties = new Properties();
+        try (BufferedReader reader = new BufferedReader(new FileReader("config.properties"))) {
+            properties.load(reader);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        //extract data
+        String ipAddr = properties.getProperty("ip_addr");
+        String user = properties.getProperty("username");
+        String password = properties.getProperty("password");
+        
         Node node = new Node(ipAddr, user, password);
+        //run NetJumper
         Thread thread1 = new Thread(node);
         thread1.start();
         try {
             thread1.join();
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         } finally {
             node.disconnectSSHConnection();
-            System.out.println("Please wait for last iteration");
         }
     }
 }
